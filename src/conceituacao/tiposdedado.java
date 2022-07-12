ecdf9657f188f7fe48b60e83a9f60977033b939c
larissambasso <larissa.mbasso@outlook.com>
2022-07-12 14:57:14 -0300

    adicionando exercicio2

---
+++ src/conceituacao/tiposdedado.java
@@ -0,0 +1,52 @@
+package conceituacao;
+
+public class tiposdedado {
+
+	public static void main(String[] args) {
+		byte b1 = 10;
+		byte b2 = 20;
+		
+		short c1 = 20000;
+		
+		int d1 = 28500;
+		
+		long e1 = 1000000000000000000L;
+		long e2 = 2004005000500055000L;
+		
+		float f1 = 4.5f;
+		float f2 = 10.68f;
+		
+		double g1 = 85.69d;
+		double g2 = 99.84d;
+		
+		char h1 = 'W';
+		//char h2 = 'Tw';//CHAR só aceita um caractere
+		char h3 = '\u0057'; //este código é igual a variavel h1
+		
+		String st1="Fulano";
+		String st2="Ciclano";
+		String st3="ag ettKNBJBJBB&*&%&75894389";
+		
+		boolean bo1 = true;
+		boolean bo2 = false;
+		
+		System.out.println(b1);
+		System.out.println(b2);
+		System.out.println(c1);
+		System.out.println(d1);
+		System.out.println(e1);
+		System.out.println(e2);
+		System.out.println(f1);
+		System.out.println(f2);
+		System.out.println(g1);
+		System.out.println(g2);
+		System.out.println(h1);
+		System.out.println(h3);
+		System.out.println(st1);
+		System.out.println(st2);
+		System.out.println(st3);
+		System.out.println(bo1);
+		System.out.println(bo2);
+	}
+
+}
